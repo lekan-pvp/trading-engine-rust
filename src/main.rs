67d1a8e2bc2b6c54a21c524cd1d104dea1fefa08
Владислав Попов <lekanl3k4n@yandex.ru>
2022-12-04 22:25:25 +0300

    order book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod matching_engine;
+
+use matching_engine::orderbook::{BidOrAsk, Order, OrderBook};
+
 fn main() {
-    println!("Hello, world!");
+    let buy_order_frim_alice = Order::new(BidOrAsk::Bid, 5.5);
+    let buy_order_from_bob = Order::new(BidOrAsk::Bid, 2.45);
+
+    let mut orderbook = OrderBook::new();
+    orderbook.add_order(4.4, buy_order_frim_alice);
+    orderbook.add_order(4.4, buy_order_from_bob);
+
+    let sell_order_from_alice = Order::new(BidOrAsk::Ask, 6.5);
+    orderbook.add_order(20.0, sell_order_from_alice);
+
+    println!("{:?}", orderbook);
 }
